83b9cc2ca1cebc87b3075eb41156a1084a5acc83
gunawan <gunawa414@gmail.com>
2023-08-11 18:25:45 +0700

    merevisi commit perkalian

--- fungsi.js
+++ fungsi.js
@@ -1,4 +1,11 @@
-console.log("fungsi pertama");
-
 const add = (a, b) => a + b;
-console.log(add(1, 2));
+console.log(1, 2);
+
+const subtract = (a, b) => a - b;
+console.log(subtract(1, 2));
+
+const multiply = (a, b) => a * b;
+console.log(multiply(1, 2));
+
+const divide = (a, b) => a / b;
+console.log(divide(1, 2));
--- tes1.js
+++
@@ -1,1 +0,0 @@
-console.log("Halaman test");
